51b6da4e544e65057bdc95f3f5f2a08f6a417bb3
simjaeik <wodlr2007@gmail.com>
2020-01-23 22:21:15 +0900

    체육복 문제 해결

--- Programmers/탐욕법(Greedy)/greedy/greedy/탐욕법.cpp
+++ Programmers/탐욕법(Greedy)/greedy/greedy/탐욕법.cpp
@@ -1,35 +1,48 @@
+#include <iostream>
 #include <vector>
 #include <set>
+#include <algorithm>
 
 int solution(int n, std::vector<int> lost, std::vector<int> reserve) {
 	int answer = n;
-	std::vector<int> lost_compare = lost;
+	std::set<int> lost_set;
 	std::set<int> reserve_set;
+
+	std::sort(lost.begin(), lost.end());
 
 	for (auto a : reserve)
 		reserve_set.insert(a);
 
-	while (!lost_compare.empty()) {
+	for (auto a : lost)
+		lost_set.insert(a);
 
-		if (reserve_set.find(lost_compare.back() + 1) != reserve_set.end()) {// is exists
-			reserve_set.erase(lost_compare.back() + 1);
-			lost.pop_back();
+	for (auto a : lost) {
+		if (reserve_set.find(a) != reserve_set.end() && lost_set.find(a) != lost_set.end()) {
+			reserve_set.erase(a);
+			lost_set.erase(a);
 		}
-		else if (reserve_set.find(lost_compare.back() - 1) != reserve_set.end()) {// is exists
-			reserve_set.erase(lost_compare.back() - 1);
-			lost.pop_back();
-		}
-		lost_compare.pop_back();
 	}
 
-	return answer - lost.size();
-}
+	while (!lost.empty()) {
 
+		if (reserve_set.find(lost.back() + 1) != reserve_set.end()) {// is exists
+			reserve_set.erase(lost.back() + 1);
+			lost_set.erase(lost.back());
+		}
+		else if (reserve_set.find(lost.back() - 1) != reserve_set.end()) {// is exists
+			reserve_set.erase(lost.back() - 1);
+			lost_set.erase(lost.back());
+		}
+		lost.pop_back();
+	}
+
+	return answer - lost_set.size();
+}
 int main() {
 
-	std::vector<int> a = { 2,4 };
-	std::vector<int> b = { 1,3,5 };
+	std::vector<int> a = { 2,3,4 };
+	std::vector<int> b = { 1,2,3,6 };
 
-	std::cout << solution(5, a, b);
+	std::cout << solution(7, a, b);
 
-}hi
+}
