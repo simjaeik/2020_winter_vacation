b43f05d2edc5e859666843fd16d9b3172afba115
simjaeik <wodlr2007@gmail.com>
2020-01-24 16:48:45 +0900

    가운데 글자

---
+++ Programmers/가운데 글자/middle_char/middle_char/middle.cpp
@@ -0,0 +1,17 @@
+#include <string>
+#include <iostream>
+
+std::string solution(std:: string s) {
+    std::string answer = "";
+    
+    if (s.length() % 2 == 0) 
+        answer = s.substr(s.length() / 2 - 1, 2);
+    else 
+        answer = s[s.length()/2];
+    
+    return answer;
+}
+
+int main() {
+    std::cout << solution("qweasdfasr");
+}
