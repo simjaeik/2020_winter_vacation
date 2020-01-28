9f6d63f6aa6f7bbfcff6486e53b7a8da4d7d27aa
Jae-Ik Sim <wodlr2007@gmail.com>
2020-01-28 21:20:29 +0900

    문자열 내 마음대로 정렬하기 (미완)

---
+++ Programmers/문자열 내맘대로/AHM/AHM/ahm.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <vector>
+
+int num;
+
+bool cmp(const std::string& a, const std::string& b) {
+	return a[num] <= b[num];
+}
+
+std::vector<std::string> solution(std::vector<std::string> strings, int n) {
+	std::vector<std::string> answer = strings;
+	num = n;
+
+	std::sort(answer.begin(), answer.end(), cmp);
+
+	return answer;
+}
+
+
+int main() {
+	std::vector< std::string> a = { "sun","bed","car" };
+
+	a = solution(a, 1);
+
+	for (auto b : a)
+		std::cout << b << std::endl;
+}
