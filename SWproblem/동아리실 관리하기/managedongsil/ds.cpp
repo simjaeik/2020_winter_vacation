6e405258916c1dbc0afd46bfee4b0b09247975d6
Jae-Ik Sim <wodlr2007@gmail.com>
2020-03-13 21:34:19 +0900

    섬 연결하기

--- SWproblem/동아리실 관리하기/managedongsil/ds.cpp
+++ SWproblem/동아리실 관리하기/managedongsil/ds.cpp
@@ -1,24 +1,56 @@
-#include <string>
-#include <vector>
-#include <iostream>
+#include<iostream>
+#include <ctype.h>
+
 using namespace std;
 
-int answer;
-void fibo(long prev, long now, long cnt) {
-	if (cnt < 0) {
-		answer = now;
+int N, spot[12][2], d[12][12], order[12], chk[12], answer(0);
+
+void dfs(int turn, int sum) {
+
+	if (turn > N) {
+		sum += d[order[turn - 1]][turn];
+		if (answer == 0 || answer > sum) answer = sum;
 		return;
 	}
-	fibo(now % 1234567, (prev + now )% 1234567, cnt-1);
+
+	for (int i = 1; i <= N; i++) {
+		if (chk[i]==0) {
+			if (answer != 0 && sum + d[order[turn - 1]][i] >= answer) continue;
+			chk[i] = 1;
+			order[turn] = i;
+			dfs(turn + 1, sum + d[order[turn - 1]][i]);
+			chk[i] = 0;
+		}
+	}
+
 }
 
-int solution(long n) {
-	fibo(0, 1, n-2);
-	return answer;
-}
+int main(int argc, char** argv)
+{
+	int test_case;
+	int T;
 
-int main() {
+	cin >> T;
 
-	cout << solution(10000000);
-	
+	for (test_case = 1; test_case <= T; ++test_case) {
+
+		cin >> N;
+
+		cin >> spot[0][0] >> spot[0][1];
+		cin >> spot[N + 1][0] >> spot[N + 1][1];
+
+		for (int i = 1; i <= N; i++)
+			cin >> spot[i][0] >> spot[i][1];
+
+		for (int i = 0; i <= N; i++)
+			for (int j = 1; j <= N+1; j++) {
+				d[i][j] = abs(spot[i][0] - spot[j][0]) + abs(spot[i][1] - spot[j][1]);
+			}
+
+		dfs(1, 0);
+
+		cout << "#" << test_case << " " << answer << endl;
+		answer = 0;
+	}
+	return 0;//정상종료시 반드시 0을 리턴해야합니다.
 }
--- SWproblem/최적 경로/minroute/mr.cpp
+++ SWproblem/최적 경로/minroute/mr.cpp
@@ -1,57 +1,56 @@
 #include<iostream>
-#include <vector>
-#include <algorithm>
-#include <cmath>
+#include <ctype.h>
 
 using namespace std;
 
-int start[2], home[2];
-bool cmp(pair<int, int> a, pair<int, int> b) {
+int N, spot[12][2], d[12][12], order[12], chk[12], answer(0);
 
-	double dis1 = abs(a.first - start[0]) + abs(a.second - start[1]);
-	double dis2 = abs(b.first - start[0]) + abs(b.second - start[1]);
+void dfs(int turn, int sum) {
 
-	return dis1 < dis2;
+	if (turn > N) {
+		sum += d[order[turn - 1]][turn];
+		if (answer == 0 || answer > sum) answer = sum;
+		return;
+	}
+
+	for (int i = 1; i <= N; i++) {
+		if (chk[i] == 0) {
+			if (answer != 0 && sum + d[order[turn - 1]][i] >= answer) continue;
+			chk[i] = 1;
+			order[turn] = i;
+			dfs(turn + 1, sum + d[order[turn - 1]][i]);
+			chk[i] = 0;
+		}
+	}
+
 }
 
 int main(int argc, char** argv)
 {
 	int test_case;
 	int T;
-	
+
 	cin >> T;
-	
-	for (test_case = 1; test_case <= T; ++test_case){
 
-		int N, answer(0);
+	for (test_case = 1; test_case <= T; ++test_case) {
+
 		cin >> N;
-		
-		vector<pair<int, int>> spot;
 
-		cin >> start[0] >> start[1];
-		cin >> home[0] >> home[1];
+		cin >> spot[0][0] >> spot[0][1];
+		cin >> spot[N + 1][0] >> spot[N + 1][1];
 
-		for (int i = 0; i < N; i++){
-			int x, y;
-			cin >> x >> y;
-			spot.push_back(make_pair(x, y));
-		}
-		
-		while (!spot.empty()) {
-			sort(spot.begin(), spot.end(), cmp);
-			
-			auto firs = spot[0];
-			spot.erase(spot.begin());
+		for (int i = 1; i <= N; i++)
+			cin >> spot[i][0] >> spot[i][1];
 
-			answer += abs(start[0] - firs.first) + abs(start[1] - firs.second);
+		for (int i = 0; i <= N; i++)
+			for (int j = 1; j <= N + 1; j++) {
+				d[i][j] = abs(spot[i][0] - spot[j][0]) + abs(spot[i][1] - spot[j][1]);
+			}
 
-			start[0] = firs.first;
-			start[1] = firs.second;
-		}
-		
+		dfs(1, 0);
+
 		cout << "#" << test_case << " " << answer << endl;
-
-
+		answer = 0;
 	}
 	return 0;//정상종료시 반드시 0을 리턴해야합니다.
 }
