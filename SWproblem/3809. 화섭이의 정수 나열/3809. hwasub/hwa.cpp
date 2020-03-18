486d372289b25206fd9b538ee0272c2cf8204f9b
Jae-Ik Sim <wodlr2007@gmail.com>
2020-03-18 17:40:59 +0900

    3809. 화섭이의 정수나열

---
+++ SWproblem/3809. 화섭이의 정수 나열/3809. hwasub/hwa.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+int main(int argc, char** argv)
+{
+	int test_case;
+	int T;
+
+	cin >> T;
+
+	for (test_case = 1; test_case <= T; ++test_case){
+		
+		int N;
+		string input="",ans;
+		cin >> N;
+
+		for (int i = 0; i < N; i++){
+			char tmp;
+			cin >> tmp;
+			input += tmp;
+		}
+
+		ans = "0";
+		while ( input.find(ans) != input.npos ) {
+			long long num = stoll(ans);
+			num++;
+			ans = to_string(num);
+		}
+
+		cout << "#" << test_case << " " << ans << endl;
+
+	}
+	return 0;//정상종료시 반드시 0을 리턴해야합니다.
+}
