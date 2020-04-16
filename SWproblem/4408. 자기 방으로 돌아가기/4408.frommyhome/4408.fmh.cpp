972559d8a0952082bf91c5a899b5279cbe4aed9e
Jae-Ik Sim <wodlr2007@gmail.com>
2020-04-16 22:43:03 +0900

    4408. 자기 방으로 돌아가기 - 미해결

---
+++ SWproblem/4408. 자기 방으로 돌아가기/4408.frommyhome/4408.fmh.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+#include <algorithm>
+#include<vector>
+
+using namespace std;
+
+int main(int argc, char** argv)
+{
+	int test_case;
+	int T;
+
+	cin >> T;
+
+	for (test_case = 1; test_case <= T; ++test_case) {
+		int N, ans(0);
+		cin >> N;
+
+		vector<int> room(401, 0);
+
+		for (int i = 0; i < N; i++) {
+			int st, ed;
+			cin >> st >> ed;
+			if (st > ed) swap(st, ed);
+
+			for (int j = st; j < ed; j++)
+				room[j]++;
+		}
+
+		ans = *max_element(room.begin(), room.end());
+
+		cout << "#" << test_case << " " << ans << endl;
+	}
+	return 0;//정상종료시 반드시 0을 리턴해야합니다.
+}
