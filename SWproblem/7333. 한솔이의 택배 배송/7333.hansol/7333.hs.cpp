e30261a1fa940992c835f59f56ae4b39fb76b86f
Jae-Ik Sim <wodlr2007@gmail.com>
2020-04-17 15:03:44 +0900

    7333. 한솔이의 택배 아르바이트

---
+++ SWproblem/7333. 한솔이의 택배 배송/7333.hansol/7333.hs.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+#include<vector>
+#include <algorithm>
+
+using namespace std;
+
+int main(int argc, char** argv)
+{
+	/*int test_case;
+	int T;
+
+	cin >> T;
+
+	for (test_case = 1; test_case <= T; ++test_case)
+	{
+		int N, ans(0);
+		cin >> N;
+
+		vector<int> boxes(N);
+		for (int i = 0; i < N; i++)
+			cin >> boxes[i];
+
+		sort(boxes.rbegin(), boxes.rend());
+
+		for (int i = 0; i < N; i++) {
+			int box = boxes[i];
+			if (box < 50) {
+				for (int j = 0; j < 50 / box; j++) {
+					if (!boxes.empty())
+						boxes.erase(boxes.end() - 1);
+				}
+			}
+			if (!boxes.empty())
+				boxes.erase(boxes.begin());
+			ans++;
+		}
+		cout << "#" << test_case << " " << ans << endl;
+	}*/
+
+	vector<int> a = { 3,4,5 };
+	
+	a.erase(a.end()-1);
+	
+	for (auto i : a)
+		cout << i << endl;
+
+	return 0;//정상종료시 반드시 0을 리턴해야합니다.
+}
